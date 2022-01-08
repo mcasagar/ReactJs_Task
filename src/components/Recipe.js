9e6fdde58560b873b6a9c35b855c986e8cc0298d
mcasagar <sagarbhoi97@gmail.com>
2022-01-08 09:10:04 +0530

    Initial commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,20 @@
+import React from "react";
+import RecipeItem from "./RecipeItem";
+
+function Recipes(props){
+    const {recipes} = props;
+    return(
+        <div class="row">
+            {
+                recipes.map(recipe =>( 
+                <RecipeItem 
+                name={recipe.recipe.label}  
+                image={recipe.recipe.image}
+                ingredientLines={recipe.recipe.ingredientLines} />
+                ))
+            }
+        </div>
+    );
+};
+
+export default Recipes;
